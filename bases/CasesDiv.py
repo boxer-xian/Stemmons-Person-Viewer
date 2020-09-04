f16d67cde7a08e7e74a85cd18139229987a9d322
Michael Fore <MichaelAF@BoxerProperty.com>
2020-09-04 10:42:22 -0500

    instruction update

--- bases/CasesDiv.py
+++ bases/CasesDiv.py
@@ -3,6 +3,8 @@ from dash.dependencies import Input, Output, State, MATCH, ALL
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 from db.casecalls import CaseCalls
+from handler.casehandler import CaseHander
+
 
 class Cases(Base):
     '''Class for creating and organizing cases tab, try and 
@@ -10,11 +12,18 @@ class Cases(Base):
     to keepy room. All data manipulation '''
     #you get access to the user with self.user
 
+        
+
     def current(self):
+        '''Returns current tabs and all layouts'''
         return html.Div('hello worl!', id='hello-world')
 
     def lifetime(self):
+        
         pass
 
     def relationships(self):
         pass
+
+    def create_grid(self):
+        return 'my-grid'
